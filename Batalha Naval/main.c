464d4634342c769bb4d41af7dc76ad6d1d1d261d
mariliafernandez <marilia.mfernandez@gmail.com>
2018-06-20 23:07:13 -0300

    função main

---
+++ Batalha Naval/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <locale.h>
+#include <stdlib.h>
+#include "batalhanaval.h"
+
+int main() {
+        setlocale(LC_ALL, "Portuguese");
+
+        menu();
+        return 0;
+}
